8b8307dce3cf8e102702ca119a017a8a0ce92081
Pawel Krawczyk <616047+kravietz@users.noreply.github.com>
2021-10-18 00:51:21 +0100

    Free memory allocated for arep->msg in  tac_acct_read()

--- tacc.c
+++ tacc.c
@@ -67,11 +67,11 @@ void authenticate(const struct addrinfo *tac_server, const char *tac_secret,
 
 void timeout_handler(int signum);
 
-#define    EXIT_OK        0
-#define    EXIT_FAIL    1    /* AAA failure (or server error) */
-#define    EXIT_ERR    2    /* local error */
+#define EXIT_OK 0
+#define EXIT_FAIL 1 /* AAA failure (or server error) */
+#define EXIT_ERR 2  /* local error */
 
-#define USE_SYSTEM    1
+#define USE_SYSTEM 1
 
 /* globals */
 typedef unsigned char flag;
@@ -83,55 +83,53 @@ struct utmpx utmpx;
 #endif
 
 /* take the length of a string constant without the NUL */
-#define C_STRLEN(str)    (sizeof("" str) - 1)
+#define C_STRLEN(str) (sizeof("" str) - 1)
 
 /* command line options */
 static struct option long_options[] =
-        {
-                /* operation */
-                {"authenticate", no_argument,       NULL, 'T'},
-                {"authorize",    no_argument,
-                                                    NULL, 'R'},
-                {"account",      no_argument,       NULL, 'A'},
-                {"version",
-                                 no_argument,       NULL, 'V'},
-                {"help",         no_argument,       NULL, 'h'},
+    {
+        /* operation */
+        {"authenticate", no_argument, NULL, 'T'},
+        {"authorize", no_argument,
+         NULL, 'R'},
+        {"account", no_argument, NULL, 'A'},
+        {"version",
+         no_argument, NULL, 'V'},
+        {"help", no_argument, NULL, 'h'},
 
-                /* data */
-                {"username",     required_argument, NULL, 'u'},
-                {"remote",
-                                 required_argument, NULL, 'r'},
-                {"password",     required_argument,
-                                                    NULL, 'p'},
-                {"server",       required_argument, NULL, 's'},
-                {
-                 "secret",       required_argument, NULL, 'k'},
-                {"command",
-                                 required_argument, NULL, 'c'},
-                {"exec",         required_argument,
-                                                    NULL, 'c'},
-                {"service",      required_argument, NULL, 'S'},
-                {
-                 "protocol",     required_argument, NULL, 'P'},
-                {"remote",
-                                 required_argument, NULL, 'r'},
-                {"login",        required_argument,
-                                                    NULL, 'L'},
-                {"tty",          required_argument, NULL, 'y'},
+        /* data */
+        {"username", required_argument, NULL, 'u'},
+        {"remote",
+         required_argument, NULL, 'r'},
+        {"password", required_argument,
+         NULL, 'p'},
+        {"server", required_argument, NULL, 's'},
+        {"secret", required_argument, NULL, 'k'},
+        {"command",
+         required_argument, NULL, 'c'},
+        {"exec", required_argument,
+         NULL, 'c'},
+        {"service", required_argument, NULL, 'S'},
+        {"protocol", required_argument, NULL, 'P'},
+        {"remote",
+         required_argument, NULL, 'r'},
+        {"login", required_argument,
+         NULL, 'L'},
+        {"tty", required_argument, NULL, 'y'},
 
-                /* modifiers */
-                {"quiet",        no_argument,       NULL, 'q'},
-                {"silent",       no_argument,       NULL, 'q'},
-                {"no-wtmp",      no_argument,
-                                                    NULL, 'w'},
-                {"no-encrypt",   no_argument,       NULL, 'n'},
-                {
-                 0, 0, 0,                                 0}};
+        /* modifiers */
+        {"quiet", no_argument, NULL, 'q'},
+        {"silent", no_argument, NULL, 'q'},
+        {"no-wtmp", no_argument,
+         NULL, 'w'},
+        {"no-encrypt", no_argument, NULL, 'n'},
+        {0, 0, 0, 0}};
 
 /* command line letters */
 char *opt_string = "TRAVhu:p:s:k:c:qr:wnS:P:L:y:";
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     char *pass = NULL;
     char *tty = NULL;
     char *command = NULL;
@@ -161,112 +159,124 @@ int main(int argc, char **argv) {
     flag login_mode = 0;
 
     /* check argc */
-    if (argc < 2) {
+    if (argc < 2)
+    {
         showusage(argv[0]);
         exit(EXIT_ERR);
     }
 
     /* check for login mode */
-    if (argc == 2 && isalpha(*argv[1])) {
+    if (argc == 2 && isalpha(*argv[1]))
+    {
         g_user = argv[1];
         do_author = do_authen = do_account = 1;
         command = DEFAULT_COMMAND;
         login_mode = 1;
-    } else {
+    }
+    else
+    {
         int c;
         int opt_index;
 
         while ((c = getopt_long(argc, argv, opt_string, long_options,
-                                &opt_index)) != EOF) {
-            switch (c) {
-                case 'T':
-                    do_authen = 1;
-                    break;
-                case 'R':
-                    do_author = 1;
-                    break;
-                case 'A':
-                    do_account = 1;
-                    break;
-                case 'V':
-                    showversion(argv[0]);
-                    /*NOTREACHED*/
-                    break;
-                case 'h':
-                    showusage(argv[0]);
-                    /*NOTREACHED*/
-                    break;
-                case 'u':
-                    g_user = optarg;
-                    break;
-                case 'r':
-                    remote_addr = optarg;
-                    break;
-                case 'L':
-                    // tac_login is a global variable initialized in libtac
-                    xstrcpy(tac_login, optarg, sizeof(tac_login));
-                    break;
-                case 'p':
-                    pass = optarg;
-                    break;
-                case 's':
-                    tac_server_name = optarg;
-                    break;
-                case 'k':
-                    tac_secret = optarg;
-                    break;
-                case 'c':
-                    command = optarg;
-                    break;
-                case 'S':
-                    service = optarg;
-                    break;
-                case 'P':
-                    protocol = optarg;
-                    break;
-                case 'q':
-                    quiet = 1;
-                    break;
-                case 'w':
-                    log_wtmp = 0;
-                    break;
-                case 'n':
-                    tac_encryption = 0;
-                    break;
-                case 'y':
-                    tty = optarg;
-                    break;
+                                &opt_index)) != EOF)
+        {
+            switch (c)
+            {
+            case 'T':
+                do_authen = 1;
+                break;
+            case 'R':
+                do_author = 1;
+                break;
+            case 'A':
+                do_account = 1;
+                break;
+            case 'V':
+                showversion(argv[0]);
+                /*NOTREACHED*/
+                break;
+            case 'h':
+                showusage(argv[0]);
+                /*NOTREACHED*/
+                break;
+            case 'u':
+                g_user = optarg;
+                break;
+            case 'r':
+                remote_addr = optarg;
+                break;
+            case 'L':
+                // tac_login is a global variable initialized in libtac
+                xstrcpy(tac_login, optarg, sizeof(tac_login));
+                break;
+            case 'p':
+                pass = optarg;
+                break;
+            case 's':
+                tac_server_name = optarg;
+                break;
+            case 'k':
+                tac_secret = optarg;
+                break;
+            case 'c':
+                command = optarg;
+                break;
+            case 'S':
+                service = optarg;
+                break;
+            case 'P':
+                protocol = optarg;
+                break;
+            case 'q':
+                quiet = 1;
+                break;
+            case 'w':
+                log_wtmp = 0;
+                break;
+            case 'n':
+                tac_encryption = 0;
+                break;
+            case 'y':
+                tty = optarg;
+                break;
             }
         }
     }
 
     /* check available information and set to defaults if needed */
-    if (do_authen + do_author + do_account == 0) {
+    if (do_authen + do_author + do_account == 0)
+    {
         printf("error: one of -TRAVh options is required\n");
         exit(EXIT_ERR);
     }
 
-    if (g_user == NULL) {
+    if (g_user == NULL)
+    {
         printf("error: username is required.\n");
         exit(EXIT_ERR);
     }
 
-    if (remote_addr == NULL) {
+    if (remote_addr == NULL)
+    {
         printf("error: remote address is required.\n");
         exit(EXIT_ERR);
     }
 
-    if (service == NULL) {
+    if (service == NULL)
+    {
         printf("error: service is required.\n");
         exit(EXIT_ERR);
     }
 
-    if (protocol == NULL) {
+    if (protocol == NULL)
+    {
         printf("error: protocol is required.\n");
         exit(EXIT_ERR);
     }
 
-    if (tac_server_name == NULL) {
+    if (tac_server_name == NULL)
+    {
         printf("error: server name is required.\n");
         exit(EXIT_ERR);
     }
@@ -276,18 +286,21 @@ int main(int argc, char **argv) {
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     ret = getaddrinfo(tac_server_name, "tacacs", &hints, &tac_server);
-    if (ret != 0) {
+    if (ret != 0)
+    {
         printf("error: resolving name %s: %s", tac_server_name,
                gai_strerror(ret));
         exit(EXIT_ERR);
     }
 
-    if (tac_secret == NULL) {
+    if (tac_secret == NULL)
+    {
         printf("error: server secret is required.\n");
         exit(EXIT_ERR);
     }
 
-    if (pass == NULL) {
+    if (pass == NULL)
+    {
         signal(SIGALRM, timeout_handler);
         alarm(GETPASS_TIMEOUT);
         pass = getpass(PASSWORD_PROMPT);
@@ -297,7 +310,8 @@ int main(int argc, char **argv) {
             exit(EXIT_ERR);
     }
 
-    if (tty == NULL) {
+    if (tty == NULL)
+    {
         printf("error: tty name is required.\n");
         exit(EXIT_ERR);
     }
@@ -308,14 +322,16 @@ int main(int argc, char **argv) {
     if (do_authen)
         authenticate(tac_server, tac_secret, g_user, pass, tty, remote_addr);
 
-    if (do_author) {
+    if (do_author)
+    {
         /* authorize user */
         struct tac_attrib *attr = NULL;
         tac_add_attrib(&attr, "service", service);
         tac_add_attrib(&attr, "protocol", protocol);
 
         tac_fd = tac_connect_single(tac_server, tac_secret, NULL, 60);
-        if (tac_fd < 0) {
+        if (tac_fd < 0)
+        {
             if (!quiet)
                 printf("Error connecting to TACACS+ server: %m\n");
             tac_free_attrib(&attr);
@@ -325,13 +341,15 @@ int main(int argc, char **argv) {
         tac_author_send(tac_fd, g_user, tty, remote_addr, attr);
 
         tac_author_read(tac_fd, &arep);
-        if (arep.status != AUTHOR_STATUS_PASS_ADD
-            && arep.status != AUTHOR_STATUS_PASS_REPL) {
+        if (arep.status != AUTHOR_STATUS_PASS_ADD && arep.status != AUTHOR_STATUS_PASS_REPL)
+        {
             if (!quiet)
                 printf("Authorization FAILED: %s\n", arep.msg);
             tac_free_attrib(&attr);
             exit(EXIT_FAIL);
-        } else {
+        }
+        else
+        {
             if (!quiet)
                 printf("Authorization OK: %s\n", arep.msg);
         }
@@ -343,7 +361,8 @@ int main(int argc, char **argv) {
     memset(pass, 0, strlen(pass));
     pass = NULL;
 
-    if (do_account) {
+    if (do_account)
+    {
         /* start accounting */
         struct tac_attrib *attr = NULL;
         time_t t;
@@ -364,7 +383,8 @@ int main(int argc, char **argv) {
         tac_add_attrib(&attr, "protocol", protocol);
 
         tac_fd = tac_connect_single(tac_server, tac_secret, NULL, 60);
-        if (tac_fd < 0) {
+        if (tac_fd < 0)
+        {
             if (!quiet)
                 printf("Error connecting to TACACS+ server: %m\n");
             tac_free_attrib(&attr);
@@ -375,21 +395,26 @@ int main(int argc, char **argv) {
                       attr);
 
         ret = tac_acct_read(tac_fd, &arep);
-        if (ret == 0) {
+        if (ret == 0)
+        {
             if (!quiet)
                 printf("Accounting: START failed: %s\n", arep.msg);
             syslog(LOG_INFO, "TACACS+ accounting start failed: %s", arep.msg);
-        } else if (!login_mode && !quiet)
+        }
+        else if (!login_mode && !quiet)
             printf("Accounting: START OK\n");
 
         close(tac_fd);
 
-        tac_free_attrib(&attr);
+        if (arep.msg != NULL)
+            free(arep.msg);
 
+        tac_free_attrib(&attr);
     }
 
     /* log in local utmp */
-    if (log_wtmp) {
+    if (log_wtmp)
+    {
 #if defined(HAVE_PUTUTXLINE)
         struct timeval tv;
 
@@ -412,7 +437,8 @@ int main(int argc, char **argv) {
 #endif
     }
 
-    if (command != NULL) {
+    if (command != NULL)
+    {
         int ret;
 
         syslog(LOG_DEBUG, "starting %s for %s", command, g_user);
@@ -433,9 +459,10 @@ int main(int argc, char **argv) {
         else
             syslog(LOG_NOTICE, "command exit code %u", ret);
 #else
-        pid=fork();
+        pid = fork();
 
-        if(pid == 0) {
+        if (pid == 0)
+        {
             /* child */
 
             execl(DEFAULT_COMMAND, DEFAULT_COMMAND, ARGS, NULL);
@@ -443,22 +470,25 @@ int main(int argc, char **argv) {
             _exit(EXIT_FAIL);
         }
 
-        if(pid < 0) {
+        if (pid < 0)
+        {
             /* error */
             syslog(LOG_ERR, "fork failed: %m");
             exit(EXIT_FAIL);
         }
 
-        if(pid > 0) {
+        if (pid > 0)
+        {
             /* parent */
             int st, r;
 
-            r=wait(&st);
+            r = wait(&st);
         }
 #endif
     }
 
-    if (do_account) {
+    if (do_account)
+    {
         /* stop accounting */
         struct tac_attrib *attr = NULL;
         time_t t;
@@ -471,7 +501,8 @@ int main(int argc, char **argv) {
         tac_add_attrib(&attr, "task_id", buf);
 
         tac_fd = tac_connect_single(tac_server, tac_secret, NULL, 60);
-        if (tac_fd < 0) {
+        if (tac_fd < 0)
+        {
             if (!quiet)
                 printf("Error connecting to TACACS+ server: %m\n");
             tac_free_attrib(&attr);
@@ -481,20 +512,26 @@ int main(int argc, char **argv) {
         tac_acct_send(tac_fd, TAC_PLUS_ACCT_FLAG_STOP, g_user, tty, remote_addr,
                       attr);
         ret = tac_acct_read(tac_fd, &arep);
-        if (ret == 0) {
+        if (ret == 0)
+        {
             if (!quiet)
                 printf("Accounting: STOP failed: %s", arep.msg);
             syslog(LOG_INFO, "TACACS+ accounting stop failed: %s\n", arep.msg);
-        } else if (!login_mode && !quiet)
+        }
+        else if (!login_mode && !quiet)
             printf("Accounting: STOP OK\n");
 
         close(tac_fd);
+
+        if (arep.msg != NULL)
+            free(arep.msg);
 
         tac_free_attrib(&attr);
     }
 
     /* logout from utmp */
-    if (log_wtmp) {
+    if (log_wtmp)
+    {
 #if defined(HAVE_PUTUTXLINE)
         utmpx.ut_type = DEAD_PROCESS;
         memset(utmpx.ut_line, 0, sizeof(utmpx.ut_line));
@@ -511,20 +548,23 @@ int main(int argc, char **argv) {
     exit(EXIT_OK);
 }
 
-void sighandler(int sig __Unused) {
-    (void) sig;
+void sighandler(int sig __Unused)
+{
+    (void)sig;
     TACDEBUG(LOG_DEBUG, "caught signal %d", sig);
 }
 
 void authenticate(const struct addrinfo *tac_server, const char *tac_secret,
                   const char *user, const char *pass, const char *tty,
-                  const char *remote_addr) {
+                  const char *remote_addr)
+{
     int tac_fd;
     int ret;
     struct areply arep;
 
     tac_fd = tac_connect_single(tac_server, tac_secret, NULL, 60);
-    if (tac_fd < 0) {
+    if (tac_fd < 0)
+    {
         if (!quiet)
             printf("Error connecting to TACACS+ server: %m\n");
         exit(EXIT_ERR);
@@ -533,7 +573,8 @@ void authenticate(const struct addrinfo *tac_server, const char *tac_secret,
     /* start authentication */
 
     if (tac_authen_send(tac_fd, user, pass, tty, remote_addr,
-                        TAC_PLUS_AUTHEN_LOGIN) < 0) {
+                        TAC_PLUS_AUTHEN_LOGIN) < 0)
+    {
         if (!quiet)
             printf("Error sending query to TACACS+ server\n");
         exit(EXIT_ERR);
@@ -541,9 +582,11 @@ void authenticate(const struct addrinfo *tac_server, const char *tac_secret,
 
     ret = tac_authen_read(tac_fd, &arep);
 
-    if (ret == TAC_PLUS_AUTHEN_STATUS_GETPASS) {
+    if (ret == TAC_PLUS_AUTHEN_STATUS_GETPASS)
+    {
 
-        if (tac_cont_send(tac_fd, pass) < 0) {
+        if (tac_cont_send(tac_fd, pass) < 0)
+        {
             if (!quiet)
                 printf("Error sending query to TACACS+ server\n");
             exit(EXIT_ERR);
@@ -552,7 +595,8 @@ void authenticate(const struct addrinfo *tac_server, const char *tac_secret,
         ret = tac_authen_read(tac_fd, &arep);
     }
 
-    if (ret != TAC_PLUS_AUTHEN_STATUS_PASS) {
+    if (ret != TAC_PLUS_AUTHEN_STATUS_PASS)
+    {
         if (!quiet)
             printf("Authentication FAILED: %s\n", arep.msg);
         syslog(LOG_ERR, "authentication failed for %s: %s", user, arep.msg);
@@ -567,7 +611,8 @@ void authenticate(const struct addrinfo *tac_server, const char *tac_secret,
     close(tac_fd);
 }
 
-void showusage(char *progname) {
+void showusage(char *progname)
+{
     char *a;
 
     a = rindex(progname, '/');
@@ -606,7 +651,8 @@ void showusage(char *progname) {
     exit(EXIT_ERR);
 }
 
-void showversion(char *progname) {
+void showversion(char *progname)
+{
     char *a;
 
     a = rindex(progname, '/');
@@ -617,15 +663,16 @@ void showversion(char *progname) {
     exit(EXIT_OK);
 }
 
-void timeout_handler(int signum __Unused) {
-    (void) signum;
+void timeout_handler(int signum __Unused)
+{
+    (void)signum;
     syslog(LOG_ERR, "timeout reading password from user %s", g_user);
 }
 
 #ifdef TACDEBUG_AT_RUNTIME
 void logmsg(int level __Unused, const char *fmt, ...)
 {
-    (void) level;
+    (void)level;
     va_list ap;
 
     va_start(ap, fmt);
