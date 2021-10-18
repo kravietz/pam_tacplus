e2cf4e01bff0780e4e0550b0d641b0fadff80ce9
Pawel Krawczyk <616047+kravietz@users.noreply.github.com>
2021-10-18 22:02:02 +0100

    Clean up the attribute function

--- libtac/lib/attrib.c
+++ libtac/lib/attrib.c
@@ -21,85 +21,13 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 #include "libtac.h"
 #include "xalloc.h"
 
-static int _tac_add_attrib_pair(struct tac_attrib **attr, char *name,
-                                char separator, char *value, int truncate)
+static int _tac_attrib_checks(char *name, char separator, char *value, size_t total_len, int truncate)
 {
-    struct tac_attrib *a;
-    size_t l1 = strlen(name);
-    size_t l2;
-    unsigned int attr_cnt = 0;
-    size_t total_len;
-
-    if (l1 > TAC_PLUS_ATTRIB_MAX_LEN - 1)
-    { /* take separator into account */
-        TACSYSLOG(LOG_WARNING,
-                  "%s: attribute `%s' exceeds max. %d characters, skipping",
-                  __FUNCTION__, name, TAC_PLUS_ATTRIB_MAX_LEN - 1);
-        return LIBTAC_STATUS_ATTRIB_TOO_LONG;
-    }
-
-    total_len = l1 + 1; /* "name" + "separator" */
-
-    if (value == NULL)
-    {
-        l2 = 0;
-    }
-    else
-    {
-        l2 = strlen(value);
-    }
-
-    if (l2 > TAC_PLUS_ATTRIB_MAX_LEN - total_len)
-    {
-        if (truncate)
-        {
-            l2 = TAC_PLUS_ATTRIB_MAX_LEN - total_len;
-        }
-        else
-        {
-            TACSYSLOG(LOG_WARNING,
-                      "%s: attribute `%s' total length exceeds %d characters, skipping",
-                      __FUNCTION__, name, TAC_PLUS_ATTRIB_MAX_LEN);
-            return LIBTAC_STATUS_ATTRIB_TOO_LONG;
-        }
-    }
-
-    total_len += l2;
-
-    /* initialize the list if application passed us a null pointer */
-    if (*attr == NULL)
-    {
-        /* allocate buffer for the tac_attrib chain link */
-        *attr = (struct tac_attrib *)xcalloc(1, sizeof(struct tac_attrib));
-        a = *attr;
-    }
-    else
-    {
-        /* find the last allocated block */
-        a = *attr;
-        while (a->next != NULL)
-        {
-            a = a->next; /* a holds last allocated block */
-            attr_cnt++;
-        }
-
-        if (attr_cnt + 1 >= TAC_PLUS_ATTRIB_MAX_CNT)
-        { /* take new attrib into account */
-            TACSYSLOG(LOG_WARNING,
-                      "%s: Maximum number of attributes exceeded, skipping",
-                      __FUNCTION__);
-            return LIBTAC_STATUS_ATTRIB_TOO_MANY;
-        }
-
-        /* allocate buffer for the next tac_attrib chain link */
-        a->next = (struct tac_attrib *)xcalloc(1, sizeof(struct tac_attrib));
-        a = a->next; /* set current block pointer to the new one */
-    }
-
     if (separator != '=' && separator != '*')
     {
         separator = '=';
@@ -108,18 +36,85 @@ static int _tac_add_attrib_pair(struct tac_attrib **attr, char *name,
                   __FUNCTION__, separator);
     }
 
-    /* fill the block */
-    a->attr_len = total_len;
-    /* allocate buffer for the key=value ASCIIZ string */
-    a->attr = (char *)xcalloc(1, total_len + 1);
-    /* write the attribute=value into the buffer */
-    if (snprintf(a->attr, total_len+1, "%s%c%s", name, separator, value) < (int) total_len)
+    // https://datatracker.ietf.org/doc/html/rfc8907#section-6.1
+    total_len = strlen(name) + sizeof(separator) + strlen(value);
+    if (total_len > TAC_PLUS_ATTRIB_MAX_LEN)
     {
-        TACSYSLOG(LOG_ERR, 
-                      "%s: short snprintf write (wanted %lu bytes)",
-                      __FUNCTION__, total_len);
+
+        if (truncate)
+        {
+            TACSYSLOG(LOG_WARNING,
+                      "%s: attribute `%s' exceeds max. %d characters, truncating",
+                      __FUNCTION__, name, TAC_PLUS_ATTRIB_MAX_LEN - 1);
+            total_len = TAC_PLUS_ATTRIB_MAX_LEN;
+        }
+        else
+        {
+            TACSYSLOG(LOG_WARNING,
+                      "%s: attribute `%s' exceeds max. %d characters, ignoring",
+                      __FUNCTION__, name, TAC_PLUS_ATTRIB_MAX_LEN - 1);
+            return LIBTAC_STATUS_ATTRIB_TOO_LONG;
+        }
     }
-    a->next = NULL; /* make sure next pointer is null so that it will be allocated on next call */
+
+    return 0;
+}
+
+static int _tac_add_attrib_pair(struct tac_attrib **head, char *name, char separator, char *value, int truncate)
+{
+    struct tac_attrib *current;
+    unsigned int attr_cnt = 0;
+    size_t total_len;
+    int check;
+    total_len = strlen(name) + sizeof(separator) + strlen(value);
+    check = _tac_attrib_checks(name, separator, value, total_len, truncate);
+    if (check != 0)
+        return check;
+    if (total_len > TAC_PLUS_ATTRIB_MAX_LEN)
+        total_len = TAC_PLUS_ATTRIB_MAX_LEN;
+
+    if (*head == NULL)
+    {
+        *head = (struct tac_attrib *)xcalloc(1, sizeof(struct tac_attrib));
+        current = *head;
+        current->attr_len = total_len;
+        current->attr = (char *)xcalloc(1, total_len + 1);
+        snprintf(current->attr, total_len + 1, "%s%c%s", name, separator, value);
+        current->next = NULL;
+        return 0;
+    }
+
+    current = *head;
+
+    while (current->next != NULL)
+    {
+        current = current->next;
+        attr_cnt++;
+    }
+
+    if (attr_cnt + 1 >= TAC_PLUS_ATTRIB_MAX_CNT)
+    { /* take new attrib into account */
+        TACSYSLOG(LOG_WARNING,
+                  "%s: Maximum number of attributes exceeded, skipping",
+                  __FUNCTION__);
+        return LIBTAC_STATUS_ATTRIB_TOO_MANY;
+    }
+
+    /* allocate buffer for the next tac_attrib chain link */
+    current->next = (struct tac_attrib *)xcalloc(1, sizeof(struct tac_attrib));
+
+    /* fill the block */
+    current->next->attr_len = total_len;
+    /* allocate buffer for the key=value ASCIIZ string */
+    current->next->attr = (char *)xcalloc(1, total_len + 1);
+    /* write the attribute=value into the buffer */
+    if (snprintf(current->next->attr, total_len + 1, "%s%c%s", name, separator, value) < (int)total_len)
+    {
+        TACSYSLOG(LOG_ERR,
+                  "%s: short snprintf write (wanted %lu bytes)",
+                  __FUNCTION__, total_len);
+    }
+    current->next->next = NULL; /* make sure next pointer is null so that it will be allocated on next call */
 
     return 0;
 }
