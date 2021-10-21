a4e22c82eeecea1deb234ab4fa996a10c54b5610
Pawel Krawczyk <616047+kravietz@users.noreply.github.com>
2021-10-21 22:45:02 +0100

    Fix areply field type
    Declared as unsigned, it prevented error codes from being passed in accounting functions - code -2 was converted into 254

--- libtac/include/libtac.h
+++ libtac/include/libtac.h
@@ -66,6 +66,11 @@ extern "C"
 // TACACS+ types and constants
 #include "tacplus.h"
 
+#ifndef HAVE_ABORT
+#define abort exit(EXIT_FAILURE)
+#endif
+
+
 #if defined(__clang__)
 #define __CLANG_PREREQ(maj, min) ((__clang_major__ > (maj)) || (__clang_major__ == (maj) && __clang_minor__ >= (min)))
 #else
@@ -137,7 +142,7 @@ struct areply {
      */
     gl_list_t attr;
     char *msg;
-    unsigned int status: 8;
+    int status: 8;
     unsigned int flags: 8;
     unsigned int seq_no: 8;
 };
@@ -262,7 +267,7 @@ extern int tac_add_attrib_truncate(gl_list_t attr, char *name, char *value);
 extern int tac_add_attrib_pair_truncate(gl_list_t attr, char *name,
                                         char sep, char *value);
 
-extern int tac_read_wait(int, int, int, int *);
+extern int tac_read_wait(int, int, int, time_t *);
 
 extern uint32_t _get_session_id(void);
 
