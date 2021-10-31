3f281987fbdd2e5bf070c863d4fe3715c1e14821
Pawel Krawczyk <616047+kravietz@users.noreply.github.com>
2021-10-31 20:22:01 +0000

    Add test cases for bad password and key
    Signed-off-by: Pawel Krawczyk <616047+kravietz@users.noreply.github.com>

--- tests/libtac/c-pap.c
+++ tests/libtac/c-pap.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <stdlib.h>
 
 #include <tests/tap/basic.h>
 
@@ -14,7 +15,7 @@ int main() {
     int tac_fd = 0;
     int ret;
     struct areply arep;
-    char server_name[] = "localhost";
+    char *server_name;
     char user[] = "testuser1";
     char pass[] = "testpass123";
     char tty[] = "ttyS0";
@@ -23,9 +24,13 @@ int main() {
     struct addrinfo hints;
     struct addrinfo *tac_server;
 
-    tac_secret = strdup("testkey123");
+    tac_secret = "testkey123";
 
-    plan(4);
+    server_name = getenv("TACPLUS_SERVER");
+    if (server_name == NULL)
+        server_name = "localhost";
+
+    plan_lazy();
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
@@ -41,10 +46,37 @@ int main() {
         sysbail("tac_connect_single\n");
     }
     ret = tac_authen_send(tac_fd, user, pass, tty, remote_addr, TAC_PLUS_AUTHEN_LOGIN);
-    is_bool(ret >= 0, true, "tac_authen_send PAP");
+    is_int(ret, 0, "tac_authen_send PAP");
 
     memset(&arep, 0, sizeof(arep));
     ret = tac_authen_read(tac_fd, &arep);
     is_int(ret, TAC_PLUS_AUTHEN_STATUS_PASS, "tac_authen_read PAP");
+
+    // with incorrect login credentials
+    tac_fd = tac_connect_single(tac_server, tac_secret, NULL, 60);
+    is_bool(tac_fd > 0, true, "tac_connect_single");
+    if (tac_fd <= 0) {
+        sysbail("tac_connect_single\n");
+    }
+    ret = tac_authen_send(tac_fd, user, "incorrect password", tty, remote_addr, TAC_PLUS_AUTHEN_LOGIN);
+    is_int(ret, 0, "tac_authen_send PAP");
+
+    memset(&arep, 0, sizeof(arep));
+    ret = tac_authen_read(tac_fd, &arep);
+    is_int(ret, TAC_PLUS_AUTHEN_STATUS_FAIL, "tac_authen_read PAP bad password");
+
+    // with incorrect TACACS+ key
+    tac_secret = "badkey";
+    tac_fd = tac_connect_single(tac_server, tac_secret, NULL, 60);
+    is_bool(tac_fd > 0, true, "tac_connect_single");
+    if (tac_fd <= 0) {
+        sysbail("tac_connect_single\n");
+    }
+    ret = tac_authen_send(tac_fd, user, pass, tty, remote_addr, TAC_PLUS_AUTHEN_LOGIN);
+    is_int(ret, 0, "tac_authen_send PAP");
+
+    memset(&arep, 0, sizeof(arep));
+    ret = tac_authen_read(tac_fd, &arep);
+    is_int(ret, LIBTAC_STATUS_PROTOCOL_ERR, "tac_authen_read PAP bad key");
 
 }
