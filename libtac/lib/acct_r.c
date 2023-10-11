kravietz/pam_tacplus

commit 6380c5a81ea6e10c0083c2c383801f23b950530c

Replace deprecated bcopy() by memcpy()

libtac/lib/acct_r.c
@@ -32,20 +32,22 @@
  *             LIBTAC_STATUS_PROTOCOL_ERR
  *   >= 0 : server response, see TAC_PLUS_AUTHEN_STATUS_...
  */
-int tac_acct_read_timeout(int fd, struct areply *re, unsigned long timeout) {
+int tac_acct_read_timeout(int fd, struct areply *re, unsigned long timeout)
+{
     HDR th;
     struct acct_reply *tb = NULL;
     size_t ulen_from_header, len_from_body;
     ssize_t spacket_read;
     char *msg = NULL;
-	int timeleft = 0;
+    int timeleft = 0;
     re->attr = NULL; /* unused */
     re->msg = NULL;
 
     if (tac_readtimeout_enable &&
-        tac_read_wait(fd,timeout*1000, TAC_PLUS_HDR_SIZE,&timeleft) < 0 ) {
-        TACSYSLOG(LOG_ERR,\
-            "%s: reply timeout after %lu secs", __FUNCTION__, timeout);
+        tac_read_wait(fd, timeout * 1000, TAC_PLUS_HDR_SIZE, &timeleft) < 0)
+    {
+        TACSYSLOG(LOG_ERR,
+                  "%s: reply timeout after %lu secs", __FUNCTION__, timeout);
         re->msg = xstrdup(acct_syserr_msg);
         re->status = LIBTAC_STATUS_READ_TIMEOUT;
         free(tb);
@@ -53,10 +55,11 @@ int tac_acct_read_timeout(int fd, struct areply *re, unsigned long timeout) {
     }
 
     spacket_read = read(fd, &th, TAC_PLUS_HDR_SIZE);
-    if(spacket_read  < TAC_PLUS_HDR_SIZE) {
-        TACSYSLOG(LOG_ERR,\
-            "%s: short reply header, read %zd of %u expected: %m", __FUNCTION__,\
-            spacket_read, TAC_PLUS_HDR_SIZE);
+    if (spacket_read < TAC_PLUS_HDR_SIZE)
+    {
+        TACSYSLOG(LOG_ERR,
+                  "%s: short reply header, read %zd of %u expected: %m", __FUNCTION__,
+                  spacket_read, TAC_PLUS_HDR_SIZE);
         re->msg = xstrdup(acct_syserr_msg);
         re->status = LIBTAC_STATUS_SHORT_HDR;
         free(tb);
@@ -65,32 +68,35 @@ int tac_acct_read_timeout(int fd, struct areply *re, unsigned long timeout) {
 
     /* check the reply fields in header */
     msg = _tac_check_header(&th, TAC_PLUS_ACCT);
-    if(msg != NULL) {
+    if (msg != NULL)
+    {
         re->msg = xstrdup(msg);
         re->status = LIBTAC_STATUS_PROTOCOL_ERR;
         free(tb);
-        TACDEBUG(LOG_DEBUG, "%s: exit status=%d, status message \"%s\"",\
-            __FUNCTION__, re->status, re->msg != NULL ? re->msg : "");
+        TACDEBUG(LOG_DEBUG, "%s: exit status=%d, status message \"%s\"",
+                 __FUNCTION__, re->status, re->msg != NULL ? re->msg : "");
         return re->status;
     }
 
     ulen_from_header = ntohl(th.datalength);
-    if (ulen_from_header > TAC_PLUS_MAX_PACKET_SIZE) {
-        TACSYSLOG(LOG_ERR,\
-            "%s: length declared in the packet %zu exceeds max allowed packet size %d",\
-            __FUNCTION__,\
-            ulen_from_header, TAC_PLUS_MAX_PACKET_SIZE);
-        re->status=LIBTAC_STATUS_SHORT_HDR;
+    if (ulen_from_header > TAC_PLUS_MAX_PACKET_SIZE)
+    {
+        TACSYSLOG(LOG_ERR,
+                  "%s: length declared in the packet %zu exceeds max allowed packet size %d",
+                  __FUNCTION__,
+                  ulen_from_header, TAC_PLUS_MAX_PACKET_SIZE);
+        re->status = LIBTAC_STATUS_SHORT_HDR;
         free(tb);
         return re->status;
     }
-    tb=(struct acct_reply *) xcalloc(1, ulen_from_header);
+    tb = (struct acct_reply *)xcalloc(1, ulen_from_header);
 
     /* read reply packet body */
     if (tac_readtimeout_enable &&
-        tac_read_wait(fd,timeleft,ulen_from_header,NULL) < 0 ) {
-        TACSYSLOG(LOG_ERR,\
-            "%s: reply timeout after %lu secs", __FUNCTION__, timeout);
+        tac_read_wait(fd, timeleft, ulen_from_header, NULL) < 0)
+    {
+        TACSYSLOG(LOG_ERR,
+                  "%s: reply timeout after %lu secs", __FUNCTION__, timeout);
         re->msg = xstrdup(acct_syserr_msg);
         re->status = LIBTAC_STATUS_READ_TIMEOUT;
         free(tb);
@@ -98,11 +104,12 @@ int tac_acct_read_timeout(int fd, struct areply *re, unsigned long timeout) {
     }
 
     spacket_read = read(fd, tb, ulen_from_header);
-    if(spacket_read < (ssize_t) ulen_from_header) {
-        TACSYSLOG(LOG_ERR,\
-            "%s: short reply body, read %zd of %zu: %m",\
-            __FUNCTION__,\
-			spacket_read, ulen_from_header);
+    if (spacket_read < (ssize_t)ulen_from_header)
+    {
+        TACSYSLOG(LOG_ERR,
+                  "%s: short reply body, read %zd of %zu: %m",
+                  __FUNCTION__,
+                  spacket_read, ulen_from_header);
         re->msg = xstrdup(acct_syserr_msg);
         re->status = LIBTAC_STATUS_SHORT_BODY;
         free(tb);
@@ -110,20 +117,21 @@ int tac_acct_read_timeout(int fd, struct areply *re, unsigned long timeout) {
     }
 
     /* decrypt the body */
-    _tac_crypt((unsigned char *) tb, &th);
+    _tac_crypt((unsigned char *)tb, &th);
 
     /* Convert network byte order to host byte order */
-    tb->msg_len  = ntohs(tb->msg_len);
+    tb->msg_len = ntohs(tb->msg_len);
     tb->data_len = ntohs(tb->data_len);
 
     /* check the length fields */
-    len_from_body=sizeof(tb->msg_len) + sizeof(tb->data_len) +
-        sizeof(tb->status) + tb->msg_len + tb->data_len;
+    len_from_body = sizeof(tb->msg_len) + sizeof(tb->data_len) +
+                    sizeof(tb->status) + tb->msg_len + tb->data_len;
 
-    if(ulen_from_header != len_from_body) {
-        TACSYSLOG(LOG_ERR,\
-            "%s: inconsistent reply body, incorrect key?",\
-            __FUNCTION__);
+    if (ulen_from_header != len_from_body)
+    {
+        TACSYSLOG(LOG_ERR,
+                  "%s: inconsistent reply body, incorrect key?",
+                  __FUNCTION__);
         re->msg = xstrdup(acct_syserr_msg);
         re->status = LIBTAC_STATUS_PROTOCOL_ERR;
         free(tb);
@@ -131,41 +139,49 @@ int tac_acct_read_timeout(int fd, struct areply *re, unsigned long timeout) {
     }
 
     /* save status and clean up */
-    if(tb->msg_len) {
-        msg=(char *) xcalloc(1, tb->msg_len+1);
-        bcopy((unsigned char *) tb+TAC_ACCT_REPLY_FIXED_FIELDS_SIZE, msg, tb->msg_len);
+    if (tb->msg_len)
+    {
+        msg = (char *)xcalloc(1, tb->msg_len + 1);
+        memcpy(msg, (unsigned char *)tb + TAC_ACCT_REPLY_FIXED_FIELDS_SIZE, tb->msg_len);
         msg[(int)tb->msg_len] = '\0';
-        re->msg = msg;      /* Freed by caller */
+        re->msg = msg;
+        free(msg);
     }
 
     /* server logged our request successfully */
-    if (tb->status == TAC_PLUS_ACCT_STATUS_SUCCESS) {
+    if (tb->status == TAC_PLUS_ACCT_STATUS_SUCCESS)
+    {
         TACDEBUG(LOG_DEBUG, "%s: accounted ok", __FUNCTION__);
-        if (!re->msg) re->msg = xstrdup(acct_ok_msg);
+        if (!re->msg)
+            re->msg = xstrdup(acct_ok_msg);
         re->status = tb->status;
         free(tb);
         return re->status;
     }
 
-    TACDEBUG(LOG_DEBUG, "%s: accounting failed, server reply status=%d",\
-        __FUNCTION__, tb->status);
-    switch(tb->status) {
-        case TAC_PLUS_ACCT_STATUS_FOLLOW:
-            re->status = tb->status;
-            if (!re->msg) re->msg=xstrdup(acct_fail_msg);
-            break;
+    TACDEBUG(LOG_DEBUG, "%s: accounting failed, server reply status=%d",
+             __FUNCTION__, tb->status);
+    switch (tb->status)
+    {
+    case TAC_PLUS_ACCT_STATUS_FOLLOW:
+        re->status = tb->status;
+        if (!re->msg)
+            re->msg = xstrdup(acct_fail_msg);
+        break;
 
-        case TAC_PLUS_ACCT_STATUS_ERROR:
-        default:
-            re->status = tb->status;
-            if (!re->msg) re->msg=xstrdup(acct_err_msg);
-            break;
+    case TAC_PLUS_ACCT_STATUS_ERROR:
+    default:
+        re->status = tb->status;
+        if (!re->msg)
+            re->msg = xstrdup(acct_err_msg);
+        break;
     }
 
     free(tb);
     return re->status;
 }
 
-int tac_acct_read(int fd, struct areply *re) {
-	return tac_acct_read_timeout(fd, re, tac_timeout);
+int tac_acct_read(int fd, struct areply *re)
+{
+    return tac_acct_read_timeout(fd, re, tac_timeout);
 }

libtac/lib/acct_s.c
@@ -22,18 +22,20 @@
 #include "libtac.h"
 #include "xalloc.h"
 
-char *tac_acct_flag2str(int flag) {
-    switch(flag) {
-        case TAC_PLUS_ACCT_FLAG_MORE:
-            return "more";
-        case TAC_PLUS_ACCT_FLAG_START:
-            return "start";
-        case TAC_PLUS_ACCT_FLAG_STOP:
-            return "stop";
-        case TAC_PLUS_ACCT_FLAG_WATCHDOG:
-            return "update";
-        default:
-            return "unknown";
+char *tac_acct_flag2str(int flag)
+{
+    switch (flag)
+    {
+    case TAC_PLUS_ACCT_FLAG_MORE:
+        return "more";
+    case TAC_PLUS_ACCT_FLAG_START:
+        return "start";
+    case TAC_PLUS_ACCT_FLAG_STOP:
+        return "stop";
+    case TAC_PLUS_ACCT_FLAG_WATCHDOG:
+        return "update";
+    default:
+        return "unknown";
     }
 }
 
@@ -46,65 +48,75 @@ char *tac_acct_flag2str(int flag) {
  *             LIBTAC_STATUS_ASSEMBLY_ERR   (pending impl)
  */
 int tac_acct_send(int fd, int type, const char *user, char *tty,
-    char *r_addr, struct tac_attrib *attr) {
+                  char *r_addr, struct tac_attrib *attr)
+{
 
     HDR *th;
     struct acct tb;
     unsigned char user_len, port_len, r_addr_len;
     struct tac_attrib *a;
-    int i = 0;    /* arg count */
+    int i = 0; /* arg count */
     int pkt_len = 0;
     int pktl = 0;
-    int w;    /* write count */
-    unsigned char *pkt=NULL;
-    /* unsigned char *pktp; */             /* obsolute */
+    int w; /* write count */
+    unsigned char *pkt = NULL;
+    /* unsigned char *pktp; */ /* obsolute */
     int ret = 0;
 
     th = _tac_req_header(TAC_PLUS_ACCT, 0);
 
     /* set header options */
-    th->version=TAC_PLUS_VER_0;
-    th->encryption=tac_encryption ? TAC_PLUS_ENCRYPTED_FLAG : TAC_PLUS_UNENCRYPTED_FLAG;
+    th->version = TAC_PLUS_VER_0;
+    th->encryption = tac_encryption ? TAC_PLUS_ENCRYPTED_FLAG : TAC_PLUS_UNENCRYPTED_FLAG;
 
-    TACDEBUG(LOG_DEBUG, "%s: user '%s', tty '%s', rem_addr '%s', encrypt: %s, type: %s", \
-        __FUNCTION__, user, tty, r_addr, \
-        (tac_encryption) ? "yes" : "no", \
-        tac_acct_flag2str(type));
-        
-    user_len=(unsigned char) strlen(user);
-    port_len=(unsigned char) strlen(tty);
-    r_addr_len=(unsigned char) strlen(r_addr);
+    TACDEBUG(LOG_DEBUG, "%s: user '%s', tty '%s', rem_addr '%s', encrypt: %s, type: %s",
+             __FUNCTION__, user, tty, r_addr,
+             (tac_encryption) ? "yes" : "no",
+             tac_acct_flag2str(type));
 
-    tb.flags=(unsigned char) type;
-    tb.authen_method=tac_authen_method;
-    tb.priv_lvl=tac_priv_lvl;
-    if (!*tac_login) {
+    user_len = (unsigned char)strlen(user);
+    port_len = (unsigned char)strlen(tty);
+    r_addr_len = (unsigned char)strlen(r_addr);
+
+    tb.flags = (unsigned char)type;
+    tb.authen_method = tac_authen_method;
+    tb.priv_lvl = tac_priv_lvl;
+    if (!*tac_login)
+    {
         /* default to PAP */
         tb.authen_type = TAC_PLUS_AUTHEN_TYPE_PAP;
-    } else {
-        if (strcmp(tac_login,"chap") == 0) {
-            tb.authen_type=TAC_PLUS_AUTHEN_TYPE_CHAP;
-        } else if(strcmp(tac_login,"login") == 0) {
-            tb.authen_type=TAC_PLUS_AUTHEN_TYPE_ASCII;
-        } else {
-            tb.authen_type=TAC_PLUS_AUTHEN_TYPE_PAP;
+    }
+    else
+    {
+        if (strcmp(tac_login, "chap") == 0)
+        {
+            tb.authen_type = TAC_PLUS_AUTHEN_TYPE_CHAP;
+        }
+        else if (strcmp(tac_login, "login") == 0)
+        {
+            tb.authen_type = TAC_PLUS_AUTHEN_TYPE_ASCII;
+        }
+        else
+        {
+            tb.authen_type = TAC_PLUS_AUTHEN_TYPE_PAP;
         }
     }
-    tb.authen_service=tac_authen_service;
-    tb.user_len=user_len;
-    tb.port_len=port_len;
-    tb.r_addr_len=r_addr_len;
+    tb.authen_service = tac_authen_service;
+    tb.user_len = user_len;
+    tb.port_len = port_len;
+    tb.r_addr_len = r_addr_len;
 
     /* allocate packet */
-    pkt=(unsigned char *) xcalloc(1, TAC_ACCT_REQ_FIXED_FIELDS_SIZE);
-    pkt_len=sizeof(tb);
+    pkt = (unsigned char *)xcalloc(1, TAC_ACCT_REQ_FIXED_FIELDS_SIZE);
+    pkt_len = sizeof(tb);
 
     /* fill attribute length fields */
     a = attr;
-    while (a) {
+    while (a)
+    {
         pktl = pkt_len;
         pkt_len += sizeof(a->attr_len);
-        pkt = (unsigned char*) xrealloc(pkt, pkt_len);
+        pkt = (unsigned char *)xrealloc(pkt, pkt_len);
 
         /* see comments in author_s.c
         pktp=pkt + pkt_len;
@@ -112,7 +124,7 @@ int tac_acct_send(int fd, int type, const char *user, char *tty,
         pkt = xrealloc(pkt, pkt_len);   
         */
 
-        bcopy(&a->attr_len, pkt + pktl, sizeof(a->attr_len));
+        memcpy(pkt + pktl, &a->attr_len, sizeof(a->attr_len));
         i++;
 
         a = a->next;
@@ -120,20 +132,13 @@ int tac_acct_send(int fd, int type, const char *user, char *tty,
 
     /* fill the arg count field and add the fixed fields to packet */
     tb.arg_cnt = i;
-    bcopy(&tb, pkt, TAC_ACCT_REQ_FIXED_FIELDS_SIZE);
+    memcpy(pkt, &tb, TAC_ACCT_REQ_FIXED_FIELDS_SIZE);
 
-    /*
-#define PUTATTR(data, len) \
-        pktp = pkt + pkt_len; \
-        pkt_len += len; \
-        pkt = xrealloc(pkt, pkt_len); \
-        bcopy(data, pktp, len);
-    */
-#define PUTATTR(data, len) \
-    pktl = pkt_len; \
-    pkt_len += len; \
-    pkt = (unsigned char*) xrealloc(pkt, pkt_len); \
-    bcopy(data, pkt + pktl, len);
+#define PUTATTR(data, len)                         \
+    pktl = pkt_len;                                \
+    pkt_len += len;                                \
+    pkt = (unsigned char *)xrealloc(pkt, pkt_len); \
+    memcpy(pkt + pktl, data, len);
 
     /* fill user and port fields */
     PUTATTR(user, user_len)
@@ -142,7 +147,8 @@ int tac_acct_send(int fd, int type, const char *user, char *tty,
 
     /* fill attributes */
     a = attr;
-    while(a) {
+    while (a)
+    {
         PUTATTR(a->attr, a->attr_len)
         a = a->next;
     }
@@ -153,22 +159,24 @@ int tac_acct_send(int fd, int type, const char *user, char *tty,
     /* write header */
     w = write(fd, th, TAC_PLUS_HDR_SIZE);
 
-    if(w < TAC_PLUS_HDR_SIZE) {
-        TACSYSLOG(LOG_ERR, "%s: short write on header, wrote %d of %d: %m",\
-            __FUNCTION__, w, TAC_PLUS_HDR_SIZE);
+    if (w < TAC_PLUS_HDR_SIZE)
+    {
+        TACSYSLOG(LOG_ERR, "%s: short write on header, wrote %d of %d: %m",
+                  __FUNCTION__, w, TAC_PLUS_HDR_SIZE);
         free(pkt);
         free(th);
         return LIBTAC_STATUS_WRITE_ERR;
     }
-        
+
     /* encrypt packet body  */
     _tac_crypt(pkt, th);
 
     /* write body */
-    w=write(fd, pkt, pkt_len);
-    if(w < pkt_len) {
-        TACSYSLOG(LOG_ERR, "%s: short write on body, wrote %d of %d: %m",\
-            __FUNCTION__, w, pkt_len);
+    w = write(fd, pkt, pkt_len);
+    if (w < pkt_len)
+    {
+        TACSYSLOG(LOG_ERR, "%s: short write on body, wrote %d of %d: %m",
+                  __FUNCTION__, w, pkt_len);
         ret = LIBTAC_STATUS_WRITE_ERR;
     }
 

libtac/lib/attrib.c
@@ -91,10 +91,10 @@ static int _tac_add_attrib_pair(struct tac_attrib **attr, char *name,
     /* fill the block */
     a->attr_len=total_len;
     a->attr = (char *) xcalloc(1, total_len+1);
-    bcopy(name, a->attr, l1);    /* paste name */
+    memcpy(a->attr, name, l1);    /* paste name */
     *(a->attr+l1)=sep;           /* insert seperator "[=*]" */
     if (value != NULL) {
-        bcopy(value, (a->attr+l1+1), l2); /* paste value */
+        memcpy((a->attr+l1+1), value, l2); /* paste value */
     }
     *(a->attr+total_len) = '\0';      /* add 0 for safety */
     a->next = NULL; /* make sure it's null */

libtac/lib/authen_s.c
@@ -27,28 +27,29 @@
 #include "xalloc.h"
 
 #if defined(HAVE_OPENSSL_MD5_H) && defined(HAVE_LIBCRYPTO)
-# include <openssl/md5.h>
+#include <openssl/md5.h>
 #else
-# include "md5.h"
+#include "md5.h"
 #endif
 
 /* assume digest points to a buffer MD5_LEN size */
 static void
 digest_chap(unsigned char digest[MD5_LBLOCK], uint8_t id,
-            const char *pass, unsigned pass_len,
-            const char *chal, unsigned chal_len) {
+			const char *pass, unsigned pass_len,
+			const char *chal, unsigned chal_len)
+{
 
-    MD5_CTX mdcontext;
+	MD5_CTX mdcontext;
 
-    MD5_Init(&mdcontext);
-    /* multiple calls to MD5Update() is still much less overhead
+	MD5_Init(&mdcontext);
+	/* multiple calls to MD5Update() is still much less overhead
      * than allocating a buffer and marshalling contiguous data
      * for a single call.
      */
-    MD5_Update(&mdcontext, &id, sizeof(id));
-    MD5_Update(&mdcontext, (const unsigned char *)pass, pass_len);
-    MD5_Update(&mdcontext, (const unsigned char *)chal, chal_len);
-    MD5_Final(digest, &mdcontext);
+	MD5_Update(&mdcontext, &id, sizeof(id));
+	MD5_Update(&mdcontext, (const unsigned char *)pass, pass_len);
+	MD5_Update(&mdcontext, (const unsigned char *)chal, chal_len);
+	MD5_Final(digest, &mdcontext);
 }
 
 /* this function sends a packet do TACACS+ server, asking
@@ -62,9 +63,10 @@ digest_chap(unsigned char digest[MD5_LBLOCK], uint8_t id,
  *             LIBTAC_STATUS_ASSEMBLY_ERR
  */
 int tac_authen_send(int fd, const char *user, const char *pass, const char *tty,
-		const char *r_addr, unsigned char action) {
+					const char *r_addr, unsigned char action)
+{
 
-	HDR *th; /* TACACS+ packet header */
+	HDR *th;				/* TACACS+ packet header */
 	struct authen_start tb; /* message body */
 	int user_len, pass_len, port_len, chal_len, token_len, bodylength, w;
 	int r_addr_len;
@@ -78,18 +80,20 @@ int tac_authen_send(int fd, const char *user, const char *pass, const char *tty,
 	th = _tac_req_header(TAC_PLUS_AUTHEN, 0);
 
 	/* set some header options */
-	if (!strcmp(tac_login, "login")) {
+	if (!strcmp(tac_login, "login"))
+	{
 		th->version = TAC_PLUS_VER_0;
-	} else {
+	}
+	else
+	{
 		th->version = TAC_PLUS_VER_1;
 	}
 	th->encryption =
-			tac_encryption ?
-					TAC_PLUS_ENCRYPTED_FLAG : TAC_PLUS_UNENCRYPTED_FLAG;
+		tac_encryption ? TAC_PLUS_ENCRYPTED_FLAG : TAC_PLUS_UNENCRYPTED_FLAG;
 
 	TACDEBUG(LOG_DEBUG, "%s: user '%s', tty '%s', rem_addr '%s', encrypt: %s",
-					__FUNCTION__, user, tty, r_addr,
-					(tac_encryption) ? "yes" : "no");
+			 __FUNCTION__, user, tty, r_addr,
+			 (tac_encryption) ? "yes" : "no");
 
 	/* get size of submitted data */
 	user_len = strlen(user);
@@ -98,7 +102,8 @@ int tac_authen_send(int fd, const char *user, const char *pass, const char *tty,
 	port_len = strlen(tty);
 	r_addr_len = strlen(r_addr);
 
-	if (!strcmp(tac_login, "chap")) {
+	if (!strcmp(tac_login, "chap"))
+	{
 		unsigned char digest[MD5_LBLOCK];
 
 		digest_chap(digest, id, pass, pass_len, chal, chal_len);
@@ -108,7 +113,9 @@ int tac_authen_send(int fd, const char *user, const char *pass, const char *tty,
 		token[0] = id;
 		memcpy(token + sizeof(id), chal, chal_len);
 		memcpy(token + sizeof(id) + chal_len, digest, sizeof(digest));
-	} else {
+	}
+	else
+	{
 		token = xstrdup(pass);
 		token_len = strlen(token);
 	}
@@ -116,23 +123,31 @@ int tac_authen_send(int fd, const char *user, const char *pass, const char *tty,
 	/* fill the body of message */
 	tb.action = action;
 	tb.priv_lvl = tac_priv_lvl;
-	if (!*tac_login) {
+	if (!*tac_login)
+	{
 		/* default to PAP */
 		tb.authen_type =
-				TAC_PLUS_AUTHEN_CHPASS == action ?
-						TAC_PLUS_AUTHEN_TYPE_ASCII : TAC_PLUS_AUTHEN_TYPE_PAP;
-	} else {
-		if (!strcmp(tac_login, "chap")) {
+			TAC_PLUS_AUTHEN_CHPASS == action ? TAC_PLUS_AUTHEN_TYPE_ASCII : TAC_PLUS_AUTHEN_TYPE_PAP;
+	}
+	else
+	{
+		if (!strcmp(tac_login, "chap"))
+		{
 			tb.authen_type = TAC_PLUS_AUTHEN_TYPE_CHAP;
-		} else if (!strcmp(tac_login, "login")) {
+		}
+		else if (!strcmp(tac_login, "login"))
+		{
 			tb.authen_type = TAC_PLUS_AUTHEN_TYPE_ASCII;
-		} else {
+		}
+		else
+		{
 			tb.authen_type = TAC_PLUS_AUTHEN_TYPE_PAP;
 		}
 	}
 
 	/* data field is not used in ASCII login */
-	if (tb.authen_type == TAC_PLUS_AUTHEN_TYPE_ASCII) {
+	if (tb.authen_type == TAC_PLUS_AUTHEN_TYPE_ASCII)
+	{
 		token_len = 0;
 	}
 
@@ -149,9 +164,10 @@ int tac_authen_send(int fd, const char *user, const char *pass, const char *tty,
 
 	/* we can now write the header */
 	w = write(fd, th, TAC_PLUS_HDR_SIZE);
-	if (w < 0 || w < TAC_PLUS_HDR_SIZE) {
+	if (w < 0 || w < TAC_PLUS_HDR_SIZE)
+	{
 		TACSYSLOG(
-				LOG_ERR, "%s: short write on header, wrote %d of %d: %m", __FUNCTION__, w, TAC_PLUS_HDR_SIZE);
+			LOG_ERR, "%s: short write on header, wrote %d of %d: %m", __FUNCTION__, w, TAC_PLUS_HDR_SIZE);
 		free(token);
 		free(pkt);
 		free(th);
@@ -159,24 +175,25 @@ int tac_authen_send(int fd, const char *user, const char *pass, const char *tty,
 	}
 
 	/* build the packet */
-	pkt = (unsigned char *) xcalloc(1, bodylength + 10);
+	pkt = (unsigned char *)xcalloc(1, bodylength + 10);
 
-	bcopy(&tb, pkt + pkt_len, sizeof(tb)); /* packet body beginning */
+	memcpy(pkt + pkt_len, &tb, sizeof(tb)); /* packet body beginning */
 	pkt_len += sizeof(tb);
-	bcopy(user, pkt + pkt_len, user_len); /* user */
+	memcpy(pkt + pkt_len, user, user_len); /* user */
 	pkt_len += user_len;
-	bcopy(tty, pkt + pkt_len, port_len); /* tty */
+	memcpy(pkt + pkt_len, tty, port_len); /* tty */
 	pkt_len += port_len;
-	bcopy(r_addr, pkt + pkt_len, r_addr_len); /* rem addr */
+	memcpy(pkt + pkt_len, r_addr, r_addr_len); /* rem addr */
 	pkt_len += r_addr_len;
 
-	bcopy(token, pkt + pkt_len, token_len); /* password */
+	memcpy(pkt + pkt_len, token, token_len); /* password */
 	pkt_len += token_len;
 
 	/* pkt_len == bodylength ? */
-	if (pkt_len != bodylength) {
+	if (pkt_len != bodylength)
+	{
 		TACSYSLOG(
-				LOG_ERR, "%s: bodylength %d != pkt_len %d", __FUNCTION__, bodylength, pkt_len);
+			LOG_ERR, "%s: bodylength %d != pkt_len %d", __FUNCTION__, bodylength, pkt_len);
 		free(token);
 		free(pkt);
 		free(th);
@@ -187,9 +204,10 @@ int tac_authen_send(int fd, const char *user, const char *pass, const char *tty,
 	_tac_crypt(pkt, th);
 
 	w = write(fd, pkt, pkt_len);
-	if (w < 0 || w < pkt_len) {
+	if (w < 0 || w < pkt_len)
+	{
 		TACSYSLOG(
-				LOG_ERR, "%s: short write on body, wrote %d of %d: %m", __FUNCTION__, w, pkt_len);
+			LOG_ERR, "%s: short write on body, wrote %d of %d: %m", __FUNCTION__, w, pkt_len);
 		ret = LIBTAC_STATUS_WRITE_ERR;
 	}
 
@@ -199,4 +217,3 @@ int tac_authen_send(int fd, const char *user, const char *pass, const char *tty,
 	TACDEBUG(LOG_DEBUG, "%s: exit status=%d", __FUNCTION__, ret);
 	return ret;
 } /* tac_authen_send */
-

commit a6c274296900d5f32aa530c9702cb03b629d82c9

libtac: fix double free in tac_acct_read_timeout
Core dumped due:
"free(): double free detected in tcache 2"

re->msg should be freed by the caller as in comment 6380c5a81ea6, else
it will create a dangling pointer.

Fixes: 6380c5a81ea6 ("Replace deprecated bcopy() by memcpy()")
Signed-off-by: Loïc Sang <loic.sang@6wind.com>

libtac/lib/acct_r.c
@@ -139,8 +139,7 @@ int tac_acct_read_timeout(int fd, struct areply *re, unsigned long timeout)
         msg = (char *)xcalloc(1, tb->msg_len + 1);
         memcpy(msg, (unsigned char *)tb + TAC_ACCT_REPLY_FIXED_FIELDS_SIZE, tb->msg_len);
         msg[(int)tb->msg_len] = '\0';
-        re->msg = msg;
-        free(msg);
+        re->msg = msg; /* Freed by caller */
     }
 
     /* server logged our request successfully */